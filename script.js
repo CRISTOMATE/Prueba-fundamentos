9849e8b994bbc8c407cdfc7447deba91c325b1af
Maria Cristina Cristina <mcristinapalac@gmail.com>
2020-11-25 22:27:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7];
+for (var i = 1; i < numbers.length; i++) 
+{
+  console.log(numbers[i])
+}
+
+
+
+
+
+
+//for (var i = 0; i < ages.length; i++) {if (ages[i] >= 18) { console.log(ages[i]);
+// crear un commit al correo los pasos, de los archivos que cree, recorrer el arreglo e imprimir posiciones mayores a 1
